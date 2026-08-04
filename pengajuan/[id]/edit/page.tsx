6e3b45c43fcaa51f7a4d5e4fa77c4f8a8473fa3a
ppk-bpsda <ppkbpsdabatu@gmail.com>
2026-08-04 14:49:45 +0700

    Delete pengajuan directory

--- pengajuan/[id]/edit/page.tsx
+++
@@ -1,5 +0,0 @@
-import PengajuanForm from "@/components/PengajuanForm";
-
-export default function PengajuanEditPage({ params }: { params: { id: string } }) {
-  return <PengajuanForm mode="edit" pengajuanId={params.id} />;
-}
--- pengajuan/baru/page.tsx
+++
@@ -1,5 +0,0 @@
-import PengajuanForm from "@/components/PengajuanForm";
-
-export default function PengajuanBaruPage() {
-  return <PengajuanForm mode="create" />;
-}
